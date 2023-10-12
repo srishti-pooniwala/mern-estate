b83e1511b360482767057e04f25240814c7471cd
srishti-pooniwala <srishtipooniwala28@gmail.com>
2023-10-12 20:54:07 +0530

    init commit

---
+++ api/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL)
+.then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+const app = express();
+
+app.listen(3000, ()=>{
+    console.log('Server running on Port 3000 !');
+})
+
+app.use('/api/user', userRouter);
